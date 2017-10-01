f8dcaded87d1ab6187d527979705932f6923d544
Alex <lucky_is@mail.ru>
2017-10-02 02:35:25 +0300

    first ver

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,45 @@
+import sys
+from urllib.parse import urlparse
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import requests
+import whois
+
+
 def load_urls4check(path):
-    pass
+    with open(path,'r') as url_file:
+        return url_file.read().splitlines()
+
 
 def is_server_respond_with_200(url):
-    pass
+    request = requests.get(url)
+    if request.status_code == 200:
+        return "Все отлично, статус %d" % request.status_code
+    else:
+        return "Внимание!!! Что то не так, статус %d" % request.status_code
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def get_domain_expiration_date(url):
+    domain_name = urlparse(url).hostname
+    domain_info = whois.whois(domain_name)
+    exp_date = domain_info.expiration_date[0] if type(domain_info.expiration_date) is list else domain_info.expiration_date
+
+    if exp_date - relativedelta(moths=1) > datetime.now():
+        return "Домен оплачен до %s" % exp_date.strftime('%d-%d-%Y')
+    else:
+        return "Внимание!!! Нужно оплатить домен до %s" % exp_date.strftime('%d-%d-%Y')
+
+
+def format_output(url):
+    output = """{url}
+             {stat_res}
+             {domain_res}
+             """.format(url=url, 
+                        stat_res=is_server_respond_with_200(url), 
+                        domain_res=get_domain_expiration_date(url))
+    return output
+
 
 if __name__ == '__main__':
-    pass
+    for url in load_urls4check(sys.argv[1]):
+        print(format_output(url))
