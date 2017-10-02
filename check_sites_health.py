02186b61fcf76c920defd05f2853a7a22ac21ed0
Alex <lucky_is@mail.ru>
2017-10-02 14:58:29 +0300

    wally notices are fixed

--- check_sites_health.py
+++ check_sites_health.py
@@ -14,7 +14,7 @@ def load_urls4check(path):
 
 def is_server_respond_with_200(url):
     request = requests.get(url)
-    if request.status_code == 200:
+    if request.ok:
         return "Все отлично, статус %d" % request.status_code
     else:
         return "Внимание!!! Что то не так, статус %d" % request.status_code
@@ -24,17 +24,20 @@ def get_domain_expiration_date(url):
     parsed = urlparse(url)
     if not bool(parsed.netloc):
         return False, "Некорректный адрес"
-    domain_name = parsed.hostname
-    try:
-        domain_info = whois.whois(domain_name)
-        exp_date = domain_info.expiration_date[0] if type(domain_info.expiration_date) is list else domain_info.expiration_date
 
-        if exp_date - relativedelta(months=1) > datetime.now():
-            return True, "Домен оплачен до %s" % exp_date.strftime('%d-%d-%Y')
-        else:
-            return True, "Внимание!!! Нужно оплатить домен до %s" % exp_date.strftime('%d-%d-%Y')
-    except:
+    domain_name = parsed.hostname
+    domain_info = whois.whois(domain_name)
+
+    if domain_info.status is None:
         return False, "Несуществующий домен"
+
+    exp_date = domain_info.expiration_date[0] if type(domain_info.expiration_date) is list\
+                                              else domain_info.expiration_date
+
+    if exp_date - relativedelta(months=1) > datetime.now():
+        return True, "Домен оплачен до %s" % exp_date.strftime('%d-%d-%Y')
+    else:
+        return True, "Внимание!!! Нужно оплатить домен до %s" % exp_date.strftime('%d-%d-%Y')
 
 
 def format_output(url):
